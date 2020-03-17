5a77a7a4e56041e988dbbb6cb05fd21a3419b48a
Steven Daglish <s.c.daglish@gmail.com>
2020-03-17 10:55:09 +0000

    Added own keymay for dactyl manuform

--- keyboards/handwired/dactyl_manuform/4x5/config.h
+++ keyboards/handwired/dactyl_manuform/4x5/config.h
@@ -34,8 +34,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // wiring of each half
 #define MATRIX_ROW_PINS { F7, B1, B3, B2, B6 }
-// #define MATRIX_COL_PINS { B5, B4, E6, D7, C6 }
-#define MATRIX_COL_PINS { C6, D7, E6, B4, B5 }
+#define MATRIX_COL_PINS { B4, E6, D7, C6, D4 }
+//#define MATRIX_COL_PINS {C6, D7, E6, B4, B5}
 
 #define DIODE_DIRECTION COL2ROW
 
@@ -43,8 +43,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define MATRIX_HAS_GHOST
 
 // WS2812 RGB LED strip input and number of LEDs
-#define RGB_DI_PIN D3
-#define RGBLED_NUM 12
+//#define RGB_DI_PIN D3
+//#define RGBLED_NUM 12
 
 /* number of backlight levels */
 // #define BACKLIGHT_LEVELS 3
+
+#define SPLIT_USB_DETECT
+#define SPLIT_USB_TIMEOUT 2500
+
