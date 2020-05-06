2eb70036ddb673fe041970582806d71439006bfb
Steven Daglish <s.c.daglish@gmail.com>
2020-05-06 17:18:05 +0100

    Added numbers, shift-numbers, and f-keys

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/sdaglish/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/sdaglish/keymap.c
@@ -76,6 +76,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		  KC_LBRC, KC_RBRC,                             KC_ESC,  RS_BSPC,  RESET,               RESET, LW_SPC,  KC_ENT, KC_MINS,KC_EQL \
 		   ),
 
+/* Lower
+ * ,----------------------------------,                             ,----------------------------------,
+ * |      |      |  mup |      |      |                             | VOL+ |      |  up  |      | PgUp |
+ * |------+------+------+------+------|                             |-------------+------+------+------|
+ * |      | mleft| mdown|mright|      |                             | MUTE | left | down |right | PgDn |
+ * |------+------+------+------+------|                             |------|------+------+------+------|
+ * |      |      |      |      |      |                             | VOL- |  /   |  \   |  ?   |  |   |
+ * |------+------+------+-------------,                             ,-------------+------+------+------,
+ *        |      |      |                                                         | mbtn |mbtn2 |
+ *        '------+------'-------------'                             '-------------'------+------'
+ *                      |      |      |                             |      |      |
+ *                      |      |      |                             |      |      |
+ *                      |      |      |                             |      |      |
+ *                      '------+------'                             '------+------'
+ *                                    '------+------' '------+------'
+ *                                    |      |      | |      |      |
+ *                                    '------+------' '------+------'
+ *                                    |      | RESET| |      |      |
+ *                                    '------+------' '------+------'
+ */
+[_LOWER] = LAYOUT( \
+		  KC_LALT, KC_F1,   KC_F2,  KC_F3,   KC_F4,  KC_F5,                            KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,KC_RALT, \
+		  KC_LSFT, KC_1,    KC_2,   KC_3,    KC_4,   KC_5,                             KC_6,    KC_7,     KC_8,    KC_9,    KC_0,  KC_RSFT, \
+		  KC_LCTL, KC_EXLM, S(KC_2),S(KC_3), KC_DLR, KC_PERC,                          KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN,  KC_RCTRL , \
+		  KC_F11, KC_F12,                             KC_ESC,  RS_BSPC,  RESET,               RESET, LW_SPC,  KC_ENT, KC_MINS,KC_EQL \
+		   )
+
 };
 
 
