7d5dbe39bbefb9b3caf558211036abc68f649dbf
Steven Daglish <s.c.daglish@gmail.com>
2020-05-10 16:44:02 +0100

    .

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/sdaglish/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/sdaglish/keymap.c
@@ -23,6 +23,7 @@
 #define SFT_ENT  SFT_T(KC_ENT)
 #define LCTL_TAB CTL_T(KC_TAB)
 #define LGUI_TAB LGUI_T(KC_TAB)
+#define ALT_ENT ALT_T(KC_ENT)
 
 
 #define RAISE MO(_RAISE)
@@ -49,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		 KC_LALT, KC_QUOT,  KC_COMM,   KC_DOT, KC_P,   KC_Y,                                                   KC_F,   KC_G,   KC_C,   KC_R,   KC_L,  KC_LALT, \
 		 KC_LSFT, KC_A,     KC_O,      KC_E,   KC_U,   KC_I,                                                   KC_D,   KC_H,   KC_T,   KC_N,   KC_S,  KC_RSFT, \
 		 KC_LCTL, KC_SCLN,  KC_Q,      KC_J,   KC_K,   KC_X,                                                   KC_B,   KC_M,   KC_W,   KC_V,   KC_Z,  KC_RCTRL , \
-		 KC_LBRC, KC_RBRC,                             KC_ESC,  RS_BSPC,  LGUI_TAB,               KC_LGUI, LW_SPC,  KC_ENT, KC_MINS,KC_EQL \
+		 KC_LBRC, KC_RBRC,                             KC_ESC,  RS_BSPC,  LGUI_TAB,               KC_LGUI, LW_SPC,  ALT_ENT, KC_MINS,KC_EQL \
 		  ),
 /* Raise
  * ,----------------------------------,                             ,----------------------------------,
@@ -75,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		  KC_LALT, KC_QUOT,  KC_COMM,   KC_DOT, KC_P,   KC_Y,                                                   KC_F,   KC_G,   KC_C,   KC_R,   KC_L,  KC_LALT, \
 		  KC_LSFT, KC_A,     KC_O,      KC_E,   KC_U,   KC_I,                                                   KC_D,   KC_LEFT,   KC_DOWN,   KC_UP,   KC_RIGHT,  KC_RSFT, \
 		  KC_LCTL, KC_SCLN,  KC_Q,      KC_J,   KC_K,   KC_X,                                                   KC_NUHS,   KC_SLSH, KC_NUBS, KC_QUES,  S(KC_NUBS),  KC_RCTRL , \
-		  KC_LBRC, KC_RBRC,                             KC_ESC,  RS_BSPC,  KC_LGUI,              KC_LGUI, LW_SPC,  KC_ENT, KC_MINS,KC_EQL \
+		  KC_LBRC, KC_RBRC,                             KC_ESC,  RS_BSPC,  KC_LGUI,              KC_LGUI, LW_SPC,  ALT_ENT, KC_MINS,KC_EQL \
 		   ),
 
 /* Lower
@@ -102,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		  KC_TAB, KC_F1,   KC_F2,  KC_F3,   KC_F4,  KC_F5,                            KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,KC_LALT, \
 		  KC_LSFT, KC_1,    KC_2,   KC_3,    KC_4,   KC_5,                             KC_6,    KC_7,     KC_8,    KC_9,    KC_0,  KC_RSFT, \
 		  KC_LCTL, KC_EXLM, S(KC_2),S(KC_3), KC_DLR, KC_PERC,                          KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN,  KC_RCTRL , \
-		  KC_F11, KC_F12,                             KC_ESC,  RS_BSPC,  RESET,               KC_LGUI, LW_SPC,  KC_ENT, KC_MINS,KC_EQL \
+		  KC_F11, KC_F12,                             KC_ESC,  RS_BSPC,  RESET,               KC_LGUI, LW_SPC,  ALT_ENT, KC_MINS,KC_EQL \
 		   )
 
 };
